b4ceb3033be85fcb2953e96a6c4751e7adcec6ff
Edwin Fernando Ortega Diaz <efortega62@misena.edu.co>
2023-03-05 21:50:49 -0500

    Primeras pruebas de consumo de api

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-console.log("hola");
+//console.log("hola");
+
+
+
+//  IMPORTAR LA LIBRERÍA
+const fetchP = import('node-fetch').then(mod => mod.default)
+const fetch = (...args) => fetchP.then(fn => fn(...args))
+//const fetch = require("node-fetch");
+
+
+fetch("https://pokeapi.co/api/v2/pokemon/ditto").then((respuesta) => {
+    return(respuesta.json())
+}).then((resp) => {
+    console.log(resp);
+})
+
+
+
+
+
+// https://pokeapi.co/api/v2/pokemon/ditto
+
