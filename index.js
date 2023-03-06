531fd3154c72989a533c0204f063a57c0d26b358
Edwin Fernando Ortega Diaz <efortega62@misena.edu.co>
2023-03-05 22:01:21 -0500

    Update index.js

--- index.js
+++ index.js
@@ -12,11 +12,19 @@ fetch("https://pokeapi.co/api/v2/pokemon/ditto").then((respuesta) => {
     return(respuesta.json())
 }).then((resp) => {
     console.log(resp);
-})
+}).catch( err => console.log( err + "  "+ "error en la respuesta"));
 
 
+/**  INFO HELP
+ * 
+ * #Cómo fucniona fetch
+ * https://pablomonteserin.com/curso/javascript/ejemplos-api-fetch/#:~:text=La%20fetch%20API%20nos%20permite,es%20necesario%20usar%20ninguna%20librer%C3%ADa.
+ * 
+ * 
+ * 
+    */
 
 
-
+// API
 // https://pokeapi.co/api/v2/pokemon/ditto
 
