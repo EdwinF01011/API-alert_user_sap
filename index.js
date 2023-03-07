97322e3751c7cd60c02209577afc930fc23897e9
EdwinF01011 <efortega62@misena.edu.co>
2023-03-07 16:53:18 -0500

    install express + probando el uso de json

--- index.js
+++ index.js
@@ -1,18 +1,41 @@
-//console.log("hola");
 
+//  #SERVIDOR       -----------------------
+const express = require("express");
+const app = express();
 
-
-//  IMPORTAR LA LIBRERÍA
+//  #FUNCIÓN FETCH HTTP     --------------------
 const fetchP = import('node-fetch').then(mod => mod.default)
 const fetch = (...args) => fetchP.then(fn => fn(...args))
-//const fetch = require("node-fetch");
 
+app.listen(8080,()=>{
+    console.log("Server runing");
+})
+
+var alerts;
 
 fetch("https://pokeapi.co/api/v2/pokemon/ditto").then((respuesta) => {
-    return(respuesta.json())
-}).then((resp) => {
-    console.log(resp);
-}).catch( err => console.log( err + "  "+ "error en la respuesta"));
+        return(respuesta.json())
+        }).then((resp) => {
+            alerts= resp;
+        console.log(resp);
+        }).catch( err => console.log( err + "  "+ "error en la respuesta"));
+
+
+app.get("/", (req,res) => {
+    //res.send('<h1>'+ alerts.sprites.versions.generation-i.red-blue.back_default +'</h1>');
+    res.send( '<img src="' +   alerts['sprites']['versions']['generation-i']['red-blue']['back_default'] + '">'  );
+})
+
+
+//const fetch = require("node-fetch");
+
+// fetch("https://pokeapi.co/api/v2/pokemon/ditto").then((respuesta) => {
+//     return(respuesta.json())
+// }).then((resp) => {
+//     console.log(resp);
+// }).catch( err => console.log( err + "  "+ "error en la respuesta"));
+
+
 
 
 /**  INFO HELP
