bfeec77ba3557bb79230971efd50a7ec6be78f6b
EdwinF01011 <efortega62@misena.edu.co>
2023-03-07 21:07:04 -0500

    Estableciendo conexión mediante post

--- index.js
+++ index.js
@@ -1,39 +1,102 @@
 
-//  #SERVIDOR       -----------------------
+//  #SERVIDOR       --------------------
 const express = require("express");
 const app = express();
+app.listen(8080,()=>{
+    console.log( "Server running  ＼(ﾟｰﾟ＼)" );
+})
 
-//  #FUNCIÓN FETCH HTTP     --------------------
+//  #IMPORT FETCH HTTP     --------------------
 const fetchP = import('node-fetch').then(mod => mod.default)
 const fetch = (...args) => fetchP.then(fn => fn(...args))
-
-app.listen(8080,()=>{
-    console.log("Server runing");
-})
-
-var alerts;
-
-fetch("https://pokeapi.co/api/v2/pokemon/ditto").then((respuesta) => {
-        return(respuesta.json())
-        }).then((resp) => {
-            alerts= resp;
-        console.log(resp);
-        }).catch( err => console.log( err + "  "+ "error en la respuesta"));
-
-
-app.get("/", (req,res) => {
-    //res.send('<h1>'+ alerts.sprites.versions.generation-i.red-blue.back_default +'</h1>');
-    res.send( '<img src="' +   alerts['sprites']['versions']['generation-i']['red-blue']['back_default'] + '">'  );
-})
-
-
 //const fetch = require("node-fetch");
 
-// fetch("https://pokeapi.co/api/v2/pokemon/ditto").then((respuesta) => {
-//     return(respuesta.json())
-// }).then((resp) => {
-//     console.log(resp);
-// }).catch( err => console.log( err + "  "+ "error en la respuesta"));
+//  #TLS
+//https://stackoverflow.com/questions/52478069/node-fetch-disable-ssl-verification
+const https = require('https');
+const httpsAgent = new https.Agent({
+    rejectUnauthorized: true,
+  });
+
+//  #CONSTANTES & VARIABLES         --------------------
+var alerts;
+const urlLogin = 'https://192.168.10.201:50000/b1s/v1/Login';
+var login={
+    "CompanyDB": "PRUEBAS_ENERO",
+    "Password": "Milan2023*",
+    "UserName": "1005331526"
+}
+const data={};
+
+var request = new Request(urlLogin, {
+	method: 'POST',
+	body: login,
+	headers: new Headers()
+});
+
+//  #METHODS        --------------------
+fetch(urlLogin,{
+	method: 'POST',
+    headers: new Headers(),
+	body: login,
+    agent:httpsAgent
+}).then((respuesta) => {return(respuesta.json())
+        }).then((resp) => {
+            alerts= resp;
+            console.log(resp);
+        }).catch( err => console.log( err + "  "+ "catch...."));
+
+
+// // fetch("https://pokeapi.co/api/v2/pokemon/ditto").then((respuesta) => {
+// //     return(respuesta.json())
+// // }).then((resp) => {
+// //     console.log(resp);
+// // }).catch( err => console.log( err + "  "+ "error en la respuesta"));
+
+
+// fetch("https://pokeapi.co/api/v2/pokemon/ditto").then((respuesta) => {//
+//         return(respuesta.json())
+//         }).then((resp) => {
+//             alerts= resp;
+//         console.log(resp);
+//         }).catch( err => console.log( err + "  "+ "error en la respuesta"));
+
+
+// app.get("/", (req,res) => {
+//     res.send("hola");
+//     //res.send( '<img src="' +   alerts['sprites']['versions']['generation-i']['red-blue']['back_default'] + '">'  );
+// })
+
+
+//-----------------------------------------
+
+//const url = 'https://randomuser.me/api';
+
+// let data1 = {
+//   name: 'Sara'
+// }
+
+// var request = new Request("https://randomuser.me/api", {
+// 	method: 'POST',
+// 	body: data1,
+// 	headers: new Headers()
+// });
+
+// fetch(request)
+// .then((respuesta)=> {
+//     // Handle response we get from the API
+//     console.log(respuesta.json());
+// }).catch( err => console.log( err +'   '+'jodidos' ));
+
+//-----------------------------------------
+
+
+
+
+
+
+
+
 
 
 
